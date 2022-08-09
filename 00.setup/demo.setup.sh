4802686c138cb1057b2a54d4fd88d3931bb8aa32
Naveen Kendyala <subscribeken@gmail.com>
2022-08-09 16:22:40 -0400

    updated instructions

--- 00.setup/demo.setup.sh
+++ 00.setup/demo.setup.sh
@@ -1,9 +1,11 @@
 # Install the Operator
 oc apply -f configuration/argo-subscription.yaml
 
+oc apply -f configuration/app-operators.yaml
+
 # Ensure that all resoures are running for Argo before proceeding to next step
 # Create a GitOps Application
-oc apply -f configuration/streams-app.yaml
+oc apply -f configuration/app-streams.yaml
 
 
 
