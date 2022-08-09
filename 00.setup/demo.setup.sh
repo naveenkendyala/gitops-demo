1f91be6e2e2eb13263c2b946993d748d497b2a3a
Naveen Kendyala <subscribeken@gmail.com>
2022-08-09 15:05:19 -0400

    Added demo setup readme and updated subscription

--- 00.setup/demo.setup.sh
+++ 00.setup/demo.setup.sh
@@ -1,1 +1,22 @@
-# Setup the 
+# Install the Operator
+oc apply -f configuration/argo-subscription.yaml
+
+# Ensure that all resoures are running for Argo before proceeding to next step
+# Create a GitOps Application
+
+
+
+# ------------------------------------------------------------------------------------------------------------
+# Deleting the ArgoCD
+
+# Step:01
+  # Delete the Argo CD instances added to the namespace of the GitOps Operator
+  # oc delete gitopsservice cluster -n openshift-gitops
+
+# Step:02 [If Applicable]
+  # Delete the Argo CD from any other namespaces
+  # oc delete gitopsservice cluster -n <namespace>
+
+# Uninstall the Operator itself from UI
+
+# ------------------------------------------------------------------------------------------------------------
